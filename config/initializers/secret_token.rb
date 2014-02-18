1d4836f660fb9d83d2c87ff07df883c2c98f04b8
Peter Casanova <peter.casanova@comcast.net>
2014-02-18 16:51:52 -0500

    first commit after creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QAndA::Application.config.secret_key_base = '88228d51675662fc5177e9620c7a8aa534141c4b9ddf580c5ad2ca8c2f54914a159567b7b72eba231aeed48b00feed83c4d9a97975ebb39021ba5fd023677a87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QAndA::Application.config.session_store :cookie_store, key: '_q_and_a_session'
